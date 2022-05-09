50e3c4b0707765b7be87e16d488001bec054c6ed
jambam-cho <hs.cho@bucketplace.net>
2022-05-09 19:10:53 +0900

    feat: add dist

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
 const core = require('@actions/core')
 const request = require("request");
 
-const app_name = core.getInput("app_name", { required: true });
-
-function getBaseUrl() {
-    let url = process.env.BASE_URL
-    if (!url)
-        core.setFailed('There is no url defined in the environment variables')
-    if (url.endsWith('/')) url = url.slice(0, -1)
-    return url
-}
-
-function getAuthToken() {
-    const token = process.env.AUTH_TOKEN
-    if (!token)
-        core.setFailed(
-            'There is no token defined in the environment variables'
-        )
-    return token
-}
-
-//
-// function doRequest(options) {
-//     return new Promise(function (resolve, reject) {
-//         request(options, function (error, response) {
-//             if (!error && response.statusCode == 200) {
-//                 resolve(response);
-//             } else {
-//                 reject(error);
-//             }
-//         });
-//     });
-// }
 async function run(){
+    const app_name = core.getInput("app_name", { required: true });
+    const input_repo = core.getInput("input_repo", { required: false });
+
+    function getBaseUrl() {
+        let url = process.env.BASE_URL
+        if (!url)
+            core.setFailed('There is no url defined in the environment variables')
+        if (url.endsWith('/')) url = url.slice(0, -1)
+        return url
+    }
+
+    function getAuthToken() {
+        const token = process.env.AUTH_TOKEN
+        if (!token)
+            core.setFailed(
+                'There is no token defined in the environment variables'
+            )
+        return token
+    }
 
     let options;
     options = {
@@ -47,7 +35,21 @@ async function run(){
         if(error) console.log(error);
         if(!error && response.statusCode == 200){
             let data = JSON.parse(body)
-            console.log(data.detail.image_repo_list)
+            if (typeof data.detail.image_repo_list === 'undefined' || data.detail.image_repo_list.length === 0){
+                core.setFailed('There is no data for image repo.')
+            }
+            if (input_repo){
+                if (input_repo === data.detail.image_repo_list[0]){
+                    console.log("hiiii")
+                    core.setOutput('first_repo', data.detail.image_repo_list[0])
+                }
+                if (input_repo === data.detail.image_repo_list[1]){
+                    core.setOutput('second_repo', data.detail.image_repo_list[1])
+                }
+                if(!data.detail.image_repo_list.includes("input_repo")){
+                    core.setFailed('The input image repo and the image repo registered in ops-monster are different.')
+                }
+            }
         }
     });
 
