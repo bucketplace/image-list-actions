082bef4c19f69e6faddf9a2c156f817536d83bc6
jambam-cho <hs.cho@bucketplace.net>
2022-05-09 18:25:33 +0900

    feat: add dist

--- index.js
+++ index.js
@@ -33,23 +33,6 @@ function doRequest(options) {
     });
 }
 async function run(){
-    // const app_name = core.getInput("app_name", { required: true });
-    // function getBaseUrl() {
-    //     let url = process.env.BASE_URL
-    //     if (!url)
-    //         core.setFailed('There is no url defined in the environment variables')
-    //     if (url.endsWith('/')) url = url.slice(0, -1)
-    //     return url
-    // }
-    //
-    // function getAuthToken() {
-    //     const token = process.env.AUTH_TOKEN
-    //     if (!token)
-    //         core.setFailed(
-    //             'There is no token defined in the environment variables'
-    //         )
-    //     return token
-    // }
 
     let options;
     options = {
@@ -61,14 +44,10 @@ async function run(){
         },
     };
     doRequest(options).then(function (res) {
-        console.log(res.body);
+        res = res.body.json();
+        console.log(res.details.image_repo_list[0])
     });
 
-    // let res = request(options, function (error, response) {
-    //     if (error) throw new Error(error);
-    //     console.log(response.body);
-    // });
-    // console.log(res.body)
 
 }
 
