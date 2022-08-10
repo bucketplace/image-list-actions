971bd472ace1e875ea24c11e335d40b912635419
jambam-cho <hs.cho@bucketplace.net>
2022-08-10 15:08:26 +0900

    feat: add logs

--- dist/index.js
+++ dist/index.js
@@ -34809,31 +34809,38 @@ async function run(){
     let options;
     options = {
         'method': 'GET',
-        'url': `${getBaseUrl()}/api/v1/applications/${app_name}/`,
+        'url': `${getBaseUrl()}/v1/applications/${app_name}/`,
         'headers': {
             'accept': 'application/json',
             'Authorization': `Token ${getAuthToken()}`
         },
     };
     request(options, function(error, response, body){
-        if(error) console.log(error);
-        if(!error && response.statusCode == 200){
-            let data = JSON.parse(body)
-            if (typeof data.detail.image_repo_list === 'undefined' || data.detail.image_repo_list.length === 0){
-                core.setFailed('There is no data for image repo.')
-            }
-            if (`${input_repo}`){
-                if(!data.detail.image_repo_list.includes(`${input_repo}`)){
-                    core.setFailed('The input image repo and the image repo registered in ops-monster are different.')
+        if(error) {
+            console.log(error);
+        }else{
+            if(response.statusCode == 200){
+                console.log('#### Get image Repo from OpsMonster ####')
+                let data = JSON.parse(body)
+                if (typeof data.detail.image_repo_list === 'undefined' || data.detail.image_repo_list.length === 0){
+                    core.setFailed('#### There is no data for image repo. ####')
                 }
-                else{
-                    console.log('Input repo has been set as output')
-                    core.setOutput('image_repo', `${input_repo}`)
+                if (`${input_repo}`){
+                    if(!data.detail.image_repo_list.includes(`${input_repo}`)){
+                        core.setFailed('#### The input image repo and the image repo registered in ops-monster are different. ####')
+                    }
+                    else{
+                        console.log('#### Input repo has been set as output ####')
+                        core.setOutput('image_repo', `${input_repo}`)
+                    }
                 }
-            }
-            if (!`${input_repo}`){
-                console.log('first repo has been set as output')
-                core.setOutput('image_repo', data.detail.image_repo_list[0])
+                if (!`${input_repo}`){
+                    console.log('#### first repo has been set as output ####')
+                    core.setOutput('image_repo', data.detail.image_repo_list[0])
+                }
+            }else{
+                console.log('#### Please Check statusCode ####', response.statusCode)
+                console.log('#### Please Response Body ####', JSON.parse(body))
             }
         }
     });
